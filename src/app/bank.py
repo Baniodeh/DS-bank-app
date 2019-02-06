3a354ee3ec144e3005d72f4985fb75d68c334876
Baniodeh <amer.baniodeh@yahoo.com>
2019-02-06 12:32:17 +0100

    Bank funktioniert

--- src/app/bank.py
+++ src/app/bank.py
@@ -2,34 +2,14 @@ class Bank:
     def __init__(self, name):
         self.name = name
         self.accounts = []
-        self.transactions= []
+        self.transactions = []
 
     def open_account(self, account):
         self.accounts.append(account)
         return account
 
-    def add_transaction(self, * , sender, recipient, subject, amount):
+    def add_transaction(self, *, sender, recipient, subject, amount):
         assert amount > 0, 'Amount has to be greater than 0'
-        transaction= {"sender": sender, "recipient": recipient, "subject": subject, "amount": amount}
+        transaction = {'sender': sender, 'recipient': recipient, 'subject': subject, 'amount': amount}
         self.transactions.append(transaction)
         return transaction
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-

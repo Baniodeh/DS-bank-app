943fdfd233bc91cef405fec0d746ef6408364e74
Baniodeh <amer.baniodeh@yahoo.com>
2019-02-07 14:15:16 +0100

    BankLesson2mainOKandimproved

--- src/app/bank.py
+++ src/app/bank.py
@@ -1,27 +1,23 @@
 from .account import Account
 from .transaction import Transaction
 
-
 class Bank:
+
     def __init__(self, name):
-        self.name = name
+        self.name= name
         self.accounts = {}
         self.transactions = []
 
-    def open_account(self, account_object):
-        assert isinstance(account_object, Account), 'Account should be an app.Account'
-        assert account_object.number not in self.accounts.keys(), 'Account number {} already taken!'.format(
-            account_object.number)
-        self.accounts[account_object.number] = account_object
-        return self.accounts[account_object.number]
+    def open_account(self, account):
+            assert type(account) == Account, 'Account should be an app.Account'
+            assert account.number not in self.accounts, 'Account number 1 already taken!'
+            self.accounts[account.number] = account
+            return account
 
-    def add_transaction(self, sender, recipient, subject, amount):
-        assert sender.number in self.accounts.keys(), 'Sender has no account yet!'
-        assert recipient.number in self.accounts.keys(), 'Recipient has no account yet!'
-        transaction = Transaction(
-            sender=sender.number,
-            recipient=recipient.number,
-            subject=subject,
-            amount=amount)
-        self.transactions.append(transaction)
-        return transaction
+    def add_transaction(self, *, sender, recipient, subject, amount):
+         assert sender.number in self.accounts, 'Sender has no account yet!'
+         assert recipient.number in self.accounts, 'Recipient has no account yet!'
+         assert amount > 0, 'Amount needs to be greater than 0'
+         transaction = Transaction(sender=sender.number, recipient=recipient.number, subject=subject, amount=amount)
+         self.transactions.append(transaction)
+         return transaction
